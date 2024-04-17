05b322bb9682445542cef25633db91b6f102031f
itssss-sweta <swetamhrjn252@gmail.com>
2024-04-17 22:54:36 +0545

    fix: change login response model

--- lib/features/authentication/data/models/authentication_model.dart
+++ lib/features/authentication/data/models/authentication_model.dart
@@ -1,8 +1,10 @@
 class AuthenticationModel {
   String? token;
-  AuthenticationModel({this.token});
+  String? username;
+  AuthenticationModel({this.token, this.username});
 
   AuthenticationModel.fromJson(Map<String, dynamic> json) {
     token = json['token'];
+    username = json['username'];
   }
 }
