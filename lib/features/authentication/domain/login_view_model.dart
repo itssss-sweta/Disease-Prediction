968db4932cc22f4bdd6b5590e4bc5e1839fe3b9f
itssss-sweta <swetamhrjn252@gmail.com>
2024-04-17 22:53:56 +0545

    feat: save login response in cache and change the return type in login view model

--- lib/features/authentication/domain/login_view_model.dart
+++ lib/features/authentication/domain/login_view_model.dart
@@ -1,3 +1,4 @@
+import 'package:dis_pred/core/cache/cache_services.dart';
 import 'package:dis_pred/features/authentication/data/models/authentication_model.dart';
 import 'package:dis_pred/features/authentication/data/repository/login_repository.dart';
 import 'package:flutter/material.dart';
@@ -5,16 +6,21 @@ import 'package:flutter/material.dart';
 class LoginViewModel extends ChangeNotifier {
   AuthenticationModel? authenticationModel;
   String? token;
+  String? name;
+
+  CacheServices cacheServices = CacheServices();
 
   Future<bool> postLoginData(
       {required String name, required String password}) async {
-    authenticationModel =
+    ({AuthenticationModel? authenticationModel, String? error}) response =
         await LoginRepository.loginUser(password: password, uname: name);
-    token = authenticationModel?.token ?? '';
-    if (token != null && token?.isNotEmpty == true) {
+    if (response.error != null) {
+      token = response.authenticationModel?.token ?? '';
+      cacheServices.saveToken(token ?? '');
+      name = response.authenticationModel?.username ?? '';
+      cacheServices.saveToken(name);
       return true;
-    } else {
-      return false;
     }
+    return false;
   }
 }
