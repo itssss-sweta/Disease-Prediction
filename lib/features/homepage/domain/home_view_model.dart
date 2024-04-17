89928b101eeeece7fa050defd3a5257be300410a
itssss-sweta <swetamhrjn252@gmail.com>
2024-04-17 22:50:13 +0545

    feat: create view model for home page

---
+++ lib/features/homepage/domain/home_view_model.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:dis_pred/features/homepage/data/models/prediction_response_model.dart';
+import 'package:dis_pred/features/homepage/data/repository/home_repository.dart';
+import 'package:flutter/material.dart';
+
+class HomeViewModel extends ChangeNotifier {
+  File? image;
+  PredictionResponseModel? predictionResponseModel;
+
+  Future<({PredictionResponseModel? responseModel, String? errorMessage})>
+      uploadImage() async {
+    ({
+      String? error,
+      PredictionResponseModel? predictionResponseModel
+    }) response = await HomeRepository.predictDisease(image: image ?? File(''));
+    if (response.error == null) {
+      predictionResponseModel = response.predictionResponseModel;
+      return (responseModel: predictionResponseModel, errorMessage: null);
+    } else {
+      return (responseModel: null, errorMessage: response.error);
+    }
+  }
+}
