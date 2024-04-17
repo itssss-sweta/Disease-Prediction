9ee8757d12c4357531c7d8ea0d5d7219af548606
itssss-sweta <swetamhrjn252@gmail.com>
2024-04-17 22:50:55 +0545

    feat: view and view model separate in home page

--- lib/features/homepage/presentation/ui/homepage.dart
+++ lib/features/homepage/presentation/ui/homepage.dart
@@ -1,17 +1,15 @@
-import 'dart:convert';
 import 'dart:io';
 import 'package:dis_pred/core/constants/colors.dart';
 import 'package:dis_pred/core/constants/sizedbox.dart';
 import 'package:dis_pred/core/constants/textstyle.dart';
-import 'package:dis_pred/features/authentication/domain/login_view_model.dart';
 import 'package:dis_pred/features/authentication/presentation/ui/components/buttons.dart';
+import 'package:dis_pred/features/homepage/domain/home_view_model.dart';
 import 'package:dis_pred/features/homepage/presentation/components/image_with_replacing_button.dart';
 import 'package:dis_pred/features/homepage/presentation/components/photo_picker_modal_bottom_sheet.dart';
-import 'package:dis_pred/features/result/presentation/result_screen.dart';
+import 'package:dis_pred/features/homepage/presentation/ui/result_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
-import 'package:http/http.dart' as http;
 
 /// HomePage consisting a photo selector
 class HomePage extends StatefulWidget {
@@ -22,64 +20,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  File? _image;
-
   /// Sets the chosen image when a photo is picked by the user.
   _setImageValue(XFile? pickedImage) {
-    // Prompt the user to choose a photo and await the result.
     if (pickedImage != null) {
       setState(() {
-        // Convert the XFile to a File object and assign it to _image of type File
-        _image = File(pickedImage.path);
+        context.read<HomeViewModel>().image = File(pickedImage.path);
       });
     }
   }
 
-  // Opens bottom model of options for picking image and picked image is returned
+  /// Opens bottom model of options for picking image and picked image is returned
   Future<XFile?> _pickImage() async {
     XFile? pickedImage = await ImagePickerService().photoChooseOption(context);
     return pickedImage;
   }
 
-  Future<void> _uploadImage(String token) async {
-    if (_image == null) {
+  Future<void> _uploadImage(BuildContext context) async {
+    if (context.read<HomeViewModel>().image == null) {
       return;
     }
-
-    final url = Uri.parse("https://eye-disease-prediction-xf00.onrender.com/api/eyepredict/");
-    final request = http.MultipartRequest('POST', url);
-
-    List<int> imageBytes = await _image!.readAsBytes();
-    Stream<List<int>> imageStream = Stream.fromIterable([imageBytes]);
-
-    request.files.add(
-      http.MultipartFile.fromBytes('image', imageBytes!,
-          filename: _image!.path.split('/').last),
-    );
-    request.headers['Authorization'] = 'Token $token';
-    try {
-      final response = await request.send();
-      if (response.statusCode == 200) {
-        final responseBody = await response.stream.bytesToString();
-        // Decode the JSON response body if applicable
-        final decodedBody = jsonDecode(responseBody);
-        // final disease = decodedBody.predictions;
-        // final img = decodedBody.path;
-        // Handle the response data as needed
-        ResultScreen.presentScreen(context,decodedBody);
-      } else {
-        print("Error uploading image $response");
+    final predictResponse = await context.read<HomeViewModel>().uploadImage();
+    if (predictResponse.errorMessage == null) {
+      if (context.mounted) {
+        ResultScreen.presentScreen(context);
       }
-    } catch (e) {
-      print("error $e");
+    } else {
+      ScaffoldMessengerState().showSnackBar(SnackBar(
+        content: Text(
+          predictResponse.errorMessage ?? '',
+        ),
+        backgroundColor: Colors.red,
+      ));
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    /// TODO: access token this way
-    String token = context.read<LoginViewModel>().token ?? '';
-
     return Scaffold(
       backgroundColor: ColorPalate.dimWhite,
       appBar: AppBar(
@@ -108,7 +84,7 @@ class _HomePageState extends State<HomePage> {
                   width: 1,
                 ),
               ),
-              child: (_image == null)
+              child: (context.read<HomeViewModel>().image == null)
                   ? Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
@@ -123,7 +99,7 @@ class _HomePageState extends State<HomePage> {
                       ],
                     )
                   : imageWithReplacingButton(
-                      imagePath: _image,
+                      imagePath: context.read<HomeViewModel>().image,
                       setImagePath: () async {
                         XFile? pickedImage = await _pickImage();
                         _setImageValue(pickedImage);
@@ -136,16 +112,11 @@ class _HomePageState extends State<HomePage> {
             color: ColorPalate.teal,
             textStyle: TextStyleCustomized.semibold16white,
             onTap: () {
-              _uploadImage(token);
+              _uploadImage(context);
             },
           )
         ],
       ),
     );
   }
-
-  // _navigateToResultScreen(String token) {
-  //   _uploadImage(token);
-  //   ResultScreen.presentScreen(context);
-  // }
 }
